33d2787d77732a71f5cfc49cc3b2c2c173e82a93
NurRuzailahh <93510995+NurRuzailahh@users.noreply.github.com>
2022-02-08 15:37:26 +0800

    Update DA_GROUP10.py

--- DA_GROUP10.py
+++ DA_GROUP10.py
@@ -1,15 +1,24 @@
-import scrapy
+import socket
 
-class NewSpider(scrapy.Spider):
-  name = "new_spider"
-  start_urls = ['https://brickset.com/sets/year-2009']
+# Define the host as a tuple
 
-  def parse(self,response):
-   xpath_selector = '//img'
-   for x in response.xpath(xpath_selector):
-     newsel = '@src'
-     yield {
-       'Image Link':x.xpath(newsel).extract_first()
-     }
-   
- 
+HOST, PORT = "", 8080,
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST,PORT))
+s.listen(True)
+
+print("Serving HTTP on port %s.." %PORT);
+
+while True:
+    client_connection, client_address = s.accept();
+    request = client_connection.recv(1024);  # Buffer Size
+    print(request.decode("utf-8"));  # Display the HTTP request
+    http_response = """/
+
+HTTP/1.1 200 OK
+
+Welcome to Group10 Website !
+"""
+    client_connection.sendall(bytes(http_response, "utf-8"))
+    client_connection.close();
