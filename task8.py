6fe8e3db93971dcc957fc0ebb9f25f7882a9ec4c
seaneatsicecream <93513826+seaneatsicecream@users.noreply.github.com>
2022-02-22 15:56:16 +0800

    Add files via upload

---
+++ task8.py
@@ -0,0 +1,11 @@
+#This is to import the unittest program
+import unittest
+#This is to test the following application specifically to a particular set of inputs
+class scrapytest(unittest.TestCase):
+
+    def test_EngineType(self):
+        print ("Testing")
+# To execute commands from this module
+if __name__ == '__main__':
+    unittest.main()
+
